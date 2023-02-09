5d960eb73a19fa81eb09d70ea9ba6f22a0e5fbc0
Soroka Dmitriy <dima.soroka.2014@mail.ru>
2023-02-09 22:55:24 +1000

    Created output csv into json

---
+++ formatConversion.py
@@ -0,0 +1,23 @@
+import pandas
+import csv
+import json
+
+data = {}
+data['emoji'] = []
+
+with open("emoji_data.csv", newline="", encoding='utf-8') as csvFile:
+    csvReader = csv.reader(csvFile, delimiter=',')
+    next(csvReader, None)
+    for i in csvReader:
+        print(type(i))
+        print(i[0])
+
+        data['emoji'].append({
+            'emoji': i[0],
+            'unicode': i[1],
+            'bytes': i[2],
+            'description': i[3]
+        })
+
+with open('json.json', 'wt') as outFile:
+    json.dump(data, outFile)
