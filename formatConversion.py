fd0e691fa58c9bd4537b393feb9db7c179c8081d
Soroka Dmitriy <dima.soroka.2014@mail.ru>
2023-02-16 20:48:59 +1000

    Added xml conversion

--- formatConversion.py
+++ formatConversion.py
@@ -1,8 +1,22 @@
 import csv
 import json
+import xml.etree.ElementTree as ET
+import re
 
-data = {}
-data['car_usage'] = []
+
+def convert_row(row):
+    return f'''<{titleCSV[0]} = "{row[0]}">
+    <{titleCSV[1]}>{row[1]}</{titleCSV[1]}>
+    <{titleCSV[2]}>{row[2]}</{titleCSV[2]}>
+    <{titleCSV[3]}>{row[3]}</{titleCSV[3]}>
+    <{titleCSV[4]}>{row[4]}</{titleCSV[4]}>
+    <{titleCSV[5]}>{row[5]}</{titleCSV[5]}>
+</{titleCSV[0]}>'''
+
+
+data = {'car_usage': []}
+xmlData = []
+titleCSV = []
 
 with open("used_car_dataset.csv", newline="", encoding='utf-8') as csvFile:
     csvReader = csv.reader(csvFile, delimiter=',')
@@ -34,3 +48,14 @@ with open('json.json') as jsonFile:
         print('city: ', i['city'])
         print('year_of_manufacture: ', i['year_of_manufacture'])
         print('')
+
+with open("used_car_dataset.csv", newline="", encoding='utf-8') as csvFile:
+    csvReader = csv.reader(csvFile)
+    titleCSV = next(csvReader, None)
+    for row in csvReader:
+        xmlData.append(row)
+    print(titleCSV)
+
+with open('xml.xml', 'wt', encoding='utf-8') as xmlFile:
+    xmlFile.write('\n'.join([convert_row(row) for row in xmlData]))
+print('\n'.join([convert_row(row) for row in xmlData]))
