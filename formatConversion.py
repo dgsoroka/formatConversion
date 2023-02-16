1462e35ec6da82ba01e664c1f635fcd6c7e2292a
Soroka Dmitriy <dima.soroka.2014@mail.ru>
2023-02-16 19:15:42 +1000

    Added instruction

--- formatConversion.py
+++ formatConversion.py
@@ -1,8 +1,13 @@
 import csv
 import json
 
+# создание пустого словаря для дальнейшей загрузки в него описания объектов как в .json
+# русское название в кавычках нужно изменить на свое
+
 data = {}
 data['emoji'] = []
+
+# считываем исходный файл
 
 with open("emoji_data.csv", newline="", encoding='utf-8') as csvFile:
     csvReader = csv.reader(csvFile, delimiter=',')
@@ -11,12 +16,16 @@ with open("emoji_data.csv", newline="", encoding='utf-8') as csvFile:
         print(type(i))
         print(i[0])
 
+        # создаем структуру, как в json, слова в кавычках меняются на названия столбцов из csv файла
+
         data['emoji'].append({
             'emoji': i[0],
             'unicode': i[1],
             'bytes': i[2],
             'description': i[3]
         })
+
+# сливаем структуру в файл json
 
 with open('json.json', 'wt') as outFile:
     json.dump(data, outFile)
